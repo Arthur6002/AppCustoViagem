7b25078dcea2560d5989eb9d5c957597b725e9d2
Arthur6002 <arthurcesardefreitas@gmail.com>
2023-09-18 11:21:22 -0300

    Main page cs linha 72

--- AppCustoViagem/AppCustoViagem/AppCustoViagem/MainPage.xaml.cs
+++ AppCustoViagem/AppCustoViagem/AppCustoViagem/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AppCustoViagem.Model;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -8,21 +9,55 @@ using Xamarin.Forms;
 
 namespace AppCustoViagem
 {
+    [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
-        public MainPage()
+        public partial cla MainPage()
         {
             InitializeComponent();
+
+            PropriedadesApp = (App)Application.Current;
         }
 
-        private void btnPedagio_Clicked(object sender, EventArgs e)
+        private async void btnPedagio_Clicked(object sender, EventArgs e)
         {
+            try
+            {
+                int qntPedagios = PropriedadesApp.ListaPedagios.Count;
 
+                PropriedadesApp.ListaPedagios.Add(new Pedagio
+                {
+                    NroPedagia = qntPedagios + 1,
+                    Valor = Convert.ToDecimal(etyValorP.Text)
+                });
+            }
+            catch(Exception ex)
+            {
+                await DisplayAlert("Ops", "Ocorreu um erro: " + ex.Message, "Ok");
+            }
         }
 
-        private void btnListaPedagio_Clicked(object sender, EventArgs e)
+        private async void btnListaPedagio_Clicked(object sender, EventArgs e)
         {
+            try
+            {
+                decimal valor_total_pedagios = PropriedadesApp.ListaPedagios.Sum(item => item.Valor);
 
+                if (string.IsNullOrEmpty(etyDistancia.Text))
+                    throw new Exception("Por favor, preencha a distancia.");
+                if (string.IsNullOrEmpty(etyConsumo.Text))
+                    throw new Exception("Por favor, preencha o consumo do veiculo.");
+                if (string.IsNullOrEmpty(etyValorC.Text))
+                    throw new Exception("Por favor, preencha o valordo combustível");
+
+                decimal consumo = Convert.ToDecimal(etyConsumo.Text);
+                decimal preco_combustivel = Convert.ToDecimal(etyValorC.Text);
+                decimal distancia = Convert.ToDecimal(etyDistancia.Text);
+            }
+            catch(Exception ex)
+            {
+
+            }
         }
 
         private void btnLimpar_Clicked(object sender, EventArgs e)
